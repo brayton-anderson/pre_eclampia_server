e40d25b1f7a05154b140305ddb9d281b0d74628a
brayton.anderson <brayton@protonmail.com>
2022-08-19 08:33:10 +0300

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,35 +1,11 @@
 const path = require('path');
-const parse = require('pg-connection-string').parse;
 
-module.exports = ({ env }) => {
-
-   if(env('NODE_ENV') === 'production'){
-     const config = parse(process.env.DATABASE_URL)
-     return {
-        connection: {
-          client: 'postgres',
-          connection: {
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            user: config.user,
-            password: config.password,
-            ssl: {
-              rejectUnauthorized: false
-            },
-          },
-          debug: false,
-        },
-     }
-   }
-    
-    return{
-      connection: {
-        client: 'sqlite',
-        connection: {
-          filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-        },
-        useNullAsDefault: true,
-      },
-    }
-};
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+    },
+    useNullAsDefault: true,
+  },
+});
